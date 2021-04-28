fe54db22dfefca08ea7efb9574427734a31c74d5
baoluong2900 <57623306+baoluong2900@users.noreply.github.com>
2021-04-28 22:49:54 +0700

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,23 @@
 import aiml
+# Đối tượng Kernel là giao diện công khai với 
+# trình thông dịch AIML
+Bot= aiml.Kernel()
+# Sử dụng phương thức 'learn' để tải 
+# nội dung của tệp AIML vào Kernel
+Bot.learn("std-startup.xml")
+# Sử dụng phương thức 'respond' để tính toán phản hồi
+# vào chuỗi đầu vào của người dùng. response () trả lại 
+# câu trả lời của thông dịch viên, trong trường hợp này 
+Bot.respond("load aiml b")
 
-kernel = aiml.Kernel()
-kernel.learn("std-startup.xml")
-kernel.respond("load aiml b")
 
+# Vòng lặp vô hạn, đọc đầu vào của người dùng từ dòng lệnh 
+# và in phản hồi. 
 while True:
-      print( kernel.respond(input("Enter your message >> ")))
+    #  print( kernel.respond(input("Human >> ")))
+    message=input(">Human: ")
+    if message== "exit" :
+        exit()
+    else:
+        response=Bot.respond(message)
+        print(">Bot: "+response)
